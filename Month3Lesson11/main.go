34d71b94c9c399d615f65b0ef7f48565541e087a
davlatabdullaev <davlatdavlat0206@gmail.com>
2024-02-25 01:32:55 +0500

    middleware added

---
+++ Month3Lesson11/main.go
@@ -0,0 +1,37 @@
+
+func authenticateMiddleware(c *gin.Context) {
+	auth := c.GetHeader("Authorization")
+	if auth == "" {
+		c.AbortWithError(http.StatusUnauthorized, errors.New("unauthorized"))
+	} else {
+		c.Next()
+	}
+}
+
+func traceRequest(c *gin.Context) {
+	beforeRequest(c)
+
+	c.Next()
+
+	afterRequest(c)
+}
+
+func beforeRequest(c *gin.Context) {
+	startTime := time.Now()
+
+	c.Set("start_time", startTime)
+
+	log.Println("start time:", startTime.Format("2006-01-02 15:04:05.0000"), "path:", c.Request.URL.Path)
+}
+
+func afterRequest(c *gin.Context) {
+	startTime, exists := c.Get("start_time")
+	if !exists {
+		startTime = time.Now()
+	}
+
+	duration := time.Since(startTime.(time.Time)).Seconds()
+
+	log.Println("end time:", time.Now().Format("2006-01-02 15:04:05.0000"), "duration:", duration, "method:", c.Request.Method)
+	fmt.Println()
+}
